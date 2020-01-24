9efdbe107367ddd6ce884f4bb379926a297e73da
b-jesch <birger.jesch@gmail.com>
2020-01-24 13:52:56 +0100

    WIP: Kodinerds Community freeTV Highlights Scraper

---
+++ resources/lib/knftv.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+from tools import *
+from dateutil import parser
+from xml.etree import ElementTree as ET
+
+class Scraper():
+
+    def __init__(self):
+
+        # Properties
+
+        self.enabled = True
+        self.baseurl = 'https://www.quarantine.hs-mittweida.de/~jesch/knftv'
+        self.lang = 'de_DE'
+        self.rssurl = 'https://www.quarantine.hs-mittweida.de/~jesch/knftv/broadcasts.php'
+        self.friendlyname = 'Kodinerds Community freeTV Highlights'
+        self.shortname = 'Kodinerds freeTV'
+        self.icon = 'klack.png'
+        self.selector = '<knftv>'
+        self.detailselector = None
+        self.err404 = 'klackde_dummy.jpg'
+
+
+    def reset(self):
+
+        # Items
+
+        self.channel = ''
+        self.title = ''
+        self.thumb = False
+        self.detailURL = ''
+        self.startdate = ''
+        self.enddate = ''
+        self.runtime = '0'
+        self.genre = ''
+        self.plot = ''
+        self.cast = ''
+        self.rating = ''
+
+
+    def scrapeRSS(self, content):
+
+        self.reset()
+        knftv = ET.fromstring((self.selector + content).replace('</events>', ''))
+        try:
+            self.channel = knftv.find('ChannelName').text
+            self.title = knftv.find('Title').text or knftv.find('EpgEventTitle').text or ''
+            self.thumb = knftv.find('Icon').text
+            self.genre = knftv.find('Genre').text
+
+            if knftv.find('Nickname').text != '':
+                self.plot = '%s\n\nEmpfohlen von %s' % (knftv.find('Plot').text, knftv.find('Nickname').text)
+            else:
+                self.plot = knftv.find('Plot').text
+
+            self.startdate = parser.parse(knftv.find('Date').text)
+            self.enddate = self.startdate.replace(hour=int(knftv.find('EndTime').text[0:2]), minute=int(knftv.find('EndTime').text[3:5]))
+            if self.startdate > self.enddate: self.enddate += datetime.timedelta(days=1)
+            self.runtime = str((self.enddate - self.startdate).seconds / 60)
+
+            self.thumb = checkResource(self.thumb, self.err404)
+
+        except Exception as e:
+            print (e)
