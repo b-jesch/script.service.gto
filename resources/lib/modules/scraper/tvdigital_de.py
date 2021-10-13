a0fc085216a6566b119f70906488aa51a2b4e5b8
b-jesch <birger.jesch@gmail.com>
2021-10-13 11:53:51 +0200

    Scraper for TV Digital updated due their website changes

--- resources/lib/modules/scraper/tvdigital_de.py
+++ resources/lib/modules/scraper/tvdigital_de.py
@@ -17,6 +17,7 @@ class Scraper():
         self.icon = 'tvd.png'
         self.selector = '<div id="content-left" class="tv-highlights">'
         self.subselector = '<div class="highlight-container">'
+        self.detailselector = '<div class="rating-container">'
         self.err404 = 'tvd_dummy.jpg'
 
     def reset(self):
@@ -42,6 +43,7 @@ class Scraper():
         try:
             self.channel = re.compile('<div class="highlight-channel">(.+?)</div>', re.DOTALL).findall(content)[0]
             self.title = re.compile('<a title="(.+?) "', re.DOTALL).findall(content)[0].split(':')[0]
+            self.detailURL = self.baseurl + re.compile('href="(.+?)" name', re.DOTALL).findall(content)[0]
         except IndexError:
             pass
         try:
@@ -51,16 +53,30 @@ class Scraper():
         try:
             _string = re.compile('<strong>(.+?)</strong>', re.DOTALL).findall(content)[0].split(' | ')
             self.genre =_string[0]
-            self.cast = (' | ').join(_string[1:-1])
-            self.runtime = int(_string[-1].split()[0])
-            self.enddate = self.startdate + datetime.timedelta(minutes=self.runtime)
+            self.runtime = int(_string[-1].split()[0]) * 60
+            self.enddate = self.startdate + datetime.timedelta(seconds=self.runtime)
         except (IndexError, ValueError):
             pass
         try:
             self.thumb = re.compile('<img src="(.+?)"', re.DOTALL).findall(content)[0]
         except (IndexError, TypeError):
             pass
+
+    def scrapeDetailPage(self, content, contentID):
+
+        if contentID in content:
+            container = content.split(contentID)
+            container.pop(0)
+            content = container[0]
         try:
-            self.plot = re.compile('<strong>(.+?)</strong>', re.DOTALL).findall(content)[1]
+            self.plot = re.compile('<p>(.+?)</p>', re.DOTALL).findall(content)[0]
+        except (IndexError, TypeError):
+            pass
+
+        try:
+            # calculate rating
+            rating = re.compile('<span itemprop="ratingValue">(.+?)</span>', re.DOTALL).findall(content)[0]
+            bestrating = re.compile('<span itemprop="bestRating">(.+?)</span>', re.DOTALL).findall(content)[0]
+            self.rating = '{0:2.1f}'.format((int(rating) / int(bestrating)) * 10)
         except (IndexError, TypeError):
             pass
