7c9edff8f0d218a3a26fc63b25a7703f07fb5afb
b-jesch <birger.jesch@gmail.com>
2025-07-15 09:54:48 +0200

    addon start delay added

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import xbmcplugin
 import xbmcvfs
 
 OPT_MDELAY = getAddonSetting('mdelay', NUM, 60)
+OPT_SDELAY = getAddonSetting('sdelay', NUM, 1000)
 OPT_ENABLE_INFO = getAddonSetting('enableinfo', BOOL)
 OPT_PREFER_HD = getAddonSetting('prefer_hd', BOOL)
 OPT_PREFERRED_SCRAPER = getAddonSetting('preferred')
@@ -322,5 +323,6 @@ if __name__ == '__main__':
         writeLog('import scraper module %s' % SCRAPER_DEFAULT)
 
     Scraper = getattr(module, 'Scraper')
+    xbmc.sleep(OPT_SDELAY)
 
     router(sys.argv[2][1:])
